5496047503b1509bd0a3c5e6b69779482b7e39b9
Jorge Lucic <jvlucic@gmail.com>
2014-03-08 14:40:43 +0100

    testing

---
+++ hook.js
@@ -0,0 +1,18 @@
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+gith({
+    repo: 'jvlucic/testHook'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'develop' )
+    {
+	console.log('received something! :D');
+            // Exec a shell script
+            execFile('./hook.sh', function(error, stdout, stderr) {
+                    // Log success in some manner
+                    console.log( 'exec complete' );
+            });
+    }
+});
---
+++ hook.sh
@@ -0,0 +1,4 @@
+cd ~/node/eurow/eurow_api
+git pull origin develop > ~/node/eurow/log.txt
+forever stop 1
+forever start ~/node/eurow/eurow_api/app.js 
