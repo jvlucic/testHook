828b658d4bd93dd2b7341d12ba9afa93ea517374
Jorge Lucic <jvlucic@gmail.com>
2014-03-08 15:06:06 +0100

    testing2

--- hook.js
+++ hook.js
@@ -12,8 +12,9 @@ gith({
             // Exec a shell script
             execFile('./hook.sh', function(error, stdout, stderr) {
                     // Log success in some manner
-		   console.log( stdout );
-		   console.log( stderr );
+		   console.log( JSON.stringify(stdout) );
+		    console.log( JSON.stringify(error) );
+		   console.log( JSON.stringify(stderr) );
 
                     console.log( 'exec complete' );
             });
